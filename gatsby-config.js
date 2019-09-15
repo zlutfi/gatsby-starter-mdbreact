faf8f36f69348069283461a6388896591e0fe1eb
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-15 11:55:54 -0400

    Updated title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter with MDBReact`,
+    title: `Gatsby Starter with MDBReact`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default-mdbreact`,
+        name: `gatsby-starter-mdbreact`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
