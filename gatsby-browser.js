788d72f4d1ac53fd6b9080a63377e9fb696e44c9
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-15 09:36:44 -0400

    Added global styles for MDBReact

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+// Global style files for MDBReact
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+import "mdbreact/dist/css/mdb.css"
