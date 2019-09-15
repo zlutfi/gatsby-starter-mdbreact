ec7403fd5a81ed76dbe7c0a901637d3628df9fa3
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-15 11:56:56 -0400

    Added Jumbotron component and buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
+import { MDBContainer, MDBBtn, MDBIcon } from "mdbreact"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Jumbotron from "../components/jumbotron"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Jumbotron />
+    <MDBContainer>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">
+        <MDBBtn color="primary" outline>
+          Go to page 2
+          <MDBIcon icon="caret-right" className="ml-2" />
+        </MDBBtn>
+      </Link>
+    </MDBContainer>
   </Layout>
 )
 
