8c77e229d853fe3e05362b0b475512ce890aaee4
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-15 11:58:53 -0400

    Added container and button

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { MDBContainer, MDBBtn, MDBIcon } from "mdbreact"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <MDBContainer className="py-5">
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">
+        <MDBBtn color="primary" outline>
+          <MDBIcon icon="caret-left" className="mr-2" />
+          Go back to the homepage
+        </MDBBtn>
+      </Link>
+    </MDBContainer>
   </Layout>
 )
 
