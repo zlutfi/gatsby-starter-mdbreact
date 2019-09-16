4ded238ff24616f8fc73afeb0aea6f2026ac43ea
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-16 17:19:00 -0400

    Updated content and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
-import { MDBContainer, MDBBtn, MDBIcon } from "mdbreact"
+import {
+  MDBContainer,
+  MDBBtn,
+  MDBIcon,
+  MDBRow,
+  MDBCol,
+  MDBBadge,
+} from "mdbreact"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -12,18 +19,43 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Jumbotron />
     <MDBContainer>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">
-        <MDBBtn color="primary" outline>
-          Go to page 2
-          <MDBIcon icon="caret-right" className="ml-2" />
-        </MDBBtn>
-      </Link>
+      <MDBRow>
+        <MDBCol size="auto" md="8">
+          <h1 className="pb-3">Hi people</h1>
+          <p className="lead grey-text">
+            Welcome to your new Gatsby site. Kick off your next, great Gatsby
+            project with this default starter. This barebones starter ships with
+            the main Gatsby configuration files you might need.
+          </p>
+          <h4 className="py-3">
+            This starter is configured for the following;
+          </h4>
+          <div>
+            <MDBBadge size="lg" color="dark" className="mr-3 mb-3 p-2">
+              Sass Ready
+            </MDBBadge>
+            <MDBBadge size="lg" color="dark" className="mr-3 mb-3 p-2">
+              Font Awesome Icons
+            </MDBBadge>
+            <MDBBadge size="lg" color="dark" className="mr-3 mb-3 p-2">
+              Progressive Web App
+            </MDBBadge>
+          </div>
+          <div className="py-5">
+            <Link to="/page-2/">
+              <MDBBtn color="primary" outline>
+                Go to page 2
+                <MDBIcon icon="caret-right" className="ml-2" />
+              </MDBBtn>
+            </Link>
+          </div>
+        </MDBCol>
+        <MDBCol size="auto" md="4" className="d-none d-md-block">
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+        </MDBCol>
+      </MDBRow>
     </MDBContainer>
   </Layout>
 )
