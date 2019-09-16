10c4213b23ac5c446f888dff59639072eca06ef8
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-16 17:17:25 -0400

    Updated description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter with MDBReact`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby + MDBReact`,
+    description: `Kick off your next, great Gatsby project with this default starter.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -30,6 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
